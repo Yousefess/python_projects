3738fa236a48ff00b087ed470284baf066753ce5
Yousefess <yousefe660@gmail.com>
2025-08-02 09:50:24 +0330

    Add Number Guesser Game

---
+++ 01 Number Guesser Game/src/utils/input_validator.py
@@ -0,0 +1,15 @@
+def get_valid_input(prompt, start, end):
+    """Get a valid integer input from the user between start and end (inclusive)."""
+    while True:
+        try:
+            user_input = int(input(prompt))
+            if start <= user_input <= end:
+                return user_input
+            else:
+                print(f"Please enter a number between {start} and {end}.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
+if __name__ == "__main__":
+    get_valid_input(1, 10)
